7b9be624793a601dbc62eee5aed2b3278974aca9
nboutin <boutwork@gmail.com>
2015-10-09 16:14:32 +0200

    Added clcd_420b

---
+++ board/stm32f4-discovery/demo/clcd_420b/clcd_demo.hpp
@@ -0,0 +1,53 @@
+/*
+ Femtin V0.0.1 - Copyright (C) 2015 Nicolas Boutin.
+ All rights reserved
+
+ This file is part of the Femtin distribution.
+
+ Femtin is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License (version 2) as published by the
+ Free Software Foundation.
+
+ Femtin is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.
+ */
+
+/// ================================================================================================
+///
+/// \file	clcd_demo.hpp
+/// \brief
+/// \date	09/10/2015
+/// \author	nboutin
+///
+/// ================================================================================================
+#ifndef DEMO_CLCD_420B_CLCD_DEMO_HPP_
+#define DEMO_CLCD_420B_CLCD_DEMO_HPP_
+
+/// === Includes	================================================================================
+/// === Namespaces	================================================================================
+
+namespace demo
+{
+
+namespace clcd_420b
+{
+/// === Forward Declarations	====================================================================
+/// === Enumerations	============================================================================
+///	=== Public Declarations	========================================================================
+
+/**
+ * Must be run in a task context
+ */
+void clcd_demo();
+
+/// === Inlines Definitions	========================================================================
+
+///	=== Non-Members Definitions	====================================================================
+
+/// ------------------------------------------------------------------------------------------------
+}
+}
+
+#endif
+/// === END OF FILE	================================================================================
