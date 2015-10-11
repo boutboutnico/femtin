d1f830471602a476ea7b18df55b05955942d4209
nboutin <boutwork@gmail.com>
2015-10-11 16:51:01 +0200

    Fixed subarray

--- femtin/array.hpp
+++ femtin/array.hpp
@@ -98,16 +98,19 @@ public:
 	/// \param[in] _count	Length of the subarray
 	Array_ptr<T> subarray(size_t _pos = 0, size_t _count = L)
 	{
-		return Array_ptr<T>(&array_[min(_pos, L)], min(L - _pos, _count));
+		assert(_pos < L);
+		assert(_count <= L - _pos);
+
+		return Array_ptr<T>(&array_[_pos], _count);
 	}
 
-	/// \brief
-	/// \param[in]	_pos	Position of the first character to include
-	/// \param[in] _count	Length of the subarray
-	Array_ptr<const T> subarray(size_t _pos = 0, size_t _count = L) const
-	{
-		return Array_ptr<const T>(&array_[min(_pos, L)], min(L - _pos, _count));
-	}
+//	/// \brief
+//	/// \param[in]	_pos	Position of the first character to include
+//	/// \param[in] _count	Length of the subarray
+//	Array_ptr<const T> subarray(size_t _pos = 0, size_t _count = L) const
+//	{
+//		return Array_ptr<const T>(&array_[min(_pos, L - 1)], min(L - _pos, _count));
+//	}
 
 	/// --- Operations	----------------------------------------------------------------------------
 
@@ -158,15 +161,15 @@ class Array_ptr
 public:
 	/// === Basic Types	============================================================================
 
+	typedef T* iterator;
 	typedef T& reference;
-	typedef const T& const_reference;
 
 	/// === Constants	============================================================================
 	/// === Public Declarations	====================================================================
 
 	/// \brief	Default constructor
 	Array_ptr()
-			: data_(NULL), size_(0)
+			: array_(NULL), size_(0)
 	{
 	}
 
@@ -175,21 +178,28 @@ public:
 	/// \warning	New code should not use this constructor
 	template<size_t L>
 	Array_ptr(T (&_array)[L])
-			: data_(&_array[0]), size_(L)
+			: array_(&_array[0]), size_(L)
 	{
 	}
 
 	/// \brief	Construct provided for compatibility with code that does not use Array_ptr
 	/// \warning	New code should not use this constructor
 	Array_ptr(T* const _data, const size_t _len)
-			: data_(_data), size_(_len)
+			: array_(_data), size_(_len)
 	{
 	}
 
 	template<size_t L>
 	Array_ptr(Array<T, L>& _array)
-			: data_(_array.begin()), size_(L)
+			: array_(_array.begin()), size_(L)
 	{
+	}
+
+	template<typename T2>
+	Array_ptr(Array_ptr<T2> _array)
+			: array_(reinterpret_cast<iterator>(_array.begin())), size_(_array.max_size())
+	{
+
 	}
 
 	/// --- Capacity	----------------------------------------------------------------------------
@@ -206,28 +216,157 @@ public:
 	inline reference operator[](size_t _pos)
 	{
 		assert(_pos < max_size());
-		return data_[_pos];
+		return array_[_pos];
 	}
 
-	inline const_reference operator[](size_t _pos) const
+	inline iterator data()
 	{
-		assert(_pos < max_size());
-		return data_[_pos];
+		return array_;
+	}
+
+	/// \brief
+	/// \param[in]	_pos	Position of the first character to include
+	/// \param[in] _count	Length of the subarray
+	Array_ptr<T> subarray(size_t _pos, size_t _count)
+	{
+		assert(_pos < size_);
+		assert(_count <= size_ - _pos);
+
+		return Array_ptr<T>(&array_[_pos], _count);
+//		return Array_ptr<T>(&array_[min(_pos, size_ - 1)], min(size_ - _pos, _count));
+	}
+
+//	/// \brief
+//	/// \param[in]	_pos	Position of the first character to include
+//	/// \param[in] _count	Length of the subarray
+//	const Array_ptr<const T> subarray(size_t _pos, size_t _count) const
+//	{
+//		return Array_ptr<const T>(&array_[min(_pos, size_ - 1)], min(size_ - _pos, _count));
+//	}
+
+	/// --- Iterators	----------------------------------------------------------------------------
+
+	inline iterator begin()
+	{
+		return array_;
+	}
+
+	inline iterator end()
+	{
+		return &array_[size_];
 	}
 
 private:
 	/// === Private Declarations	================================================================
 	/// === Private Attributes	====================================================================
 
-	T* data_;
-	size_t size_;
+	T* array_;
+	const size_t size_;
 };
 
 /// === Inlines Declarations	====================================================================
 
 /// ------------------------------------------------------------------------------------------------
+/// ================================================================================================
+/// === Class Declarations	========================================================================
 
-}/// femtin
+template<typename T>
+class Array_ptr<const T>
+{
+public:
+	/// === Basic Types	============================================================================
+
+	typedef const T* const_iterator;
+	typedef const T& const_reference;
+
+	/// === Constants	============================================================================
+	/// === Public Declarations	====================================================================
+
+	/// \brief	Default constructor
+	Array_ptr()
+			: array_(NULL), size_(0)
+	{
+	}
+
+	/// \brief	Construct provided for compatibility with code that does not use Array_ptr
+	///			It can deduce array size at compile-time.
+	/// \warning	New code should not use this constructor
+	template<size_t L>
+	Array_ptr(const T (&_array)[L])
+			: array_(&_array[0]), size_(L)
+	{
+	}
+
+	/// \brief	Construct provided for compatibility with code that does not use Array_ptr
+	/// \warning	New code should not use this constructor
+	Array_ptr(const T* const _data, const size_t _len)
+			: array_(_data), size_(_len)
+	{
+	}
+
+	template<size_t L>
+	Array_ptr(const Array<T, L>& _array)
+			: array_(_array.begin()), size_(L)
+	{
+	}
+
+	template<typename T2>
+	Array_ptr(const Array_ptr<const T2> _array)
+			: array_(reinterpret_cast<const T*>(_array.begin())), size_(_array.max_size())
+	{
+
+	}
+
+	/// --- Capacity	----------------------------------------------------------------------------
+
+	/// \brief	Returns the maximum number of elements the array is able to hold due to system
+	///			or library implementation limitations.
+	inline size_t max_size() const
+	{
+		return size_;
+	}
+
+	/// --- Accessors	----------------------------------------------------------------------------
+
+	inline const_reference operator[](size_t _pos) const
+	{
+		assert(_pos < max_size());
+		return array_[_pos];
+	}
+
+	inline const_iterator data() const
+	{
+		return array_;
+	}
+
+	/// \brief
+	/// \param[in]	_pos	Position of the first character to include
+	/// \param[in] _count	Length of the subarray
+	const Array_ptr<const T> subarray(size_t _pos, size_t _count) const
+	{
+		return Array_ptr<const T>(&array_[min(_pos, size_ - 1)], min(size_ - _pos, _count));
+	}
+
+	/// --- Iterators	----------------------------------------------------------------------------
+
+	inline const_iterator begin() const
+	{
+		return array_;
+	}
+
+	inline const_iterator end() const
+	{
+		return &array_[size_];
+	}
+
+private:
+	/// === Private Declarations	================================================================
+	/// === Private Attributes	====================================================================
+
+	const T* array_;
+	const size_t size_;
+};
+}    /// femtin
 
 #endif	/// FEMTIN_ARRAY_HPP_
 /// === END OF FILE	================================================================================
