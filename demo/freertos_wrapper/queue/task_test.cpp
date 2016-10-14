cf717c5bf7296137c08cf979a3159290f0cc8130
nboutin <boutwork@gmail.com>
2016-10-14 12:18:54 +0200

    Added queue with strut demo

--- demo/freertos_wrapper/queue/task_test.cpp
+++ demo/freertos_wrapper/queue/task_test.cpp
@@ -37,9 +37,21 @@ using namespace board::led;
 
 /// === Static Definitions
 
-static const uint32_t VAL32 = 1234;
-
+static const uint16_t VAL16 = 0x1234;
+static const uint32_t VAL32 = 0x56789ABC;
 femtin::Queue queue(2, sizeof(uint32_t));
+
+struct msg_t
+{
+  uint16_t u16;
+  uint32_t u32;
+
+  friend bool operator==(msg_t _x, msg_t _y) { return (_x.u16 == _y.u16 && _x.u32 == _y.u32); }
+};
+
+static const msg_t MSG{ VAL16, VAL32 };
+
+femtin::Queue queue_msg(2, sizeof(msg_t));
 
 /// === Public Definitions
 
@@ -50,13 +62,18 @@ Task1::Task1() : Task("Task1", STACK_SIZE, STACK_PRIORITY) {}
 void Task1::run()
 {
   uint32_t write = VAL32;
+  msg_t msg      = MSG;
 
   for (;;)
   {
     LED_Green.on();
-    LED_Blue.off();
     this_task::sleep_for(std::chrono::seconds(1));
     queue.send_to_back(&write);
+    this_task::sleep_for(std::chrono::seconds(1));
+
+    LED_Blue.on();
+    this_task::sleep_for(std::chrono::seconds(1));
+    queue_msg.send_to_back(&msg);
     this_task::sleep_for(std::chrono::seconds(1));
   }
 }
@@ -70,6 +87,7 @@ Task2::Task2() : Task("Task2", STACK_SIZE, STACK_PRIORITY) {}
 void Task2::run()
 {
   uint32_t read = 0;
+  msg_t msg_read;
 
   for (;;)
   {
@@ -78,7 +96,22 @@ void Task2::run()
       if (read == VAL32)
       {
         LED_Green.off();
-        LED_Blue.on();
+      }
+      else
+        LED_Red.on();
+    }
+    /// Timeout
+    else
+    {
+      LED_Orange.toggle();
+      this_task::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    if (queue_msg.receive(&msg_read, std::chrono::seconds(5)) == true)
+    {
+      if (msg_read == MSG)
+      {
+        LED_Blue.off();
       }
       else
         LED_Red.on();
